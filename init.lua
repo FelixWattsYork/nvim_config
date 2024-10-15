5ed054e0315fe2c5999fb6f5bf538ae3b96ab34b
FelixWattsYork <hmq514@york.ac.uk>
2024-10-15 15:09:00 +0100

    Got Luasnips from remote location working, Got Vimtex working and moved in a lot of the latex commands from ejmastnak

---
+++ init.lua
@@ -0,0 +1,53 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+-- Snippets Configuration
+require("luasnip.loaders.from_lua").load({ paths = { "~/.config/nvim/LuaSnip" } })
+require("luasnip.loaders.from_lua").load({ paths = { "~/.config/nvim/LuaSnip/tex" } })
+-- Require LuaSnip
+local luasnip = require("luasnip")
+
+luasnip.setup({
+  enable_autosnippets = true,
+})
+
+-- Configure key mappings
+vim.keymap.set({ "i", "s" }, "<Tab>", function()
+  if luasnip.expand_or_jumpable() then
+    luasnip.expand_or_jump()
+  else
+    return "<Tab>"
+  end
+end, { silent = true, expr = true })
+
+vim.keymap.set({ "i", "s" }, "<S-Tab>", function()
+  if luasnip.jumpable(-1) then
+    luasnip.jump(-1)
+  else
+    return "<S-Tab>"
+  end
+end, { silent = true, expr = true })
+
+-- Setup nvim-cmp.
+local cmp = require("cmp")
+
+cmp.setup({
+  mapping = {
+    -- Use <Tab> to expand snippets
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback() -- fall back to normal tab functionality
+      end
+    end, { "i", "s" }),
+
+    -- Use <S-Tab> to jump backward in snippets
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  },
+})
